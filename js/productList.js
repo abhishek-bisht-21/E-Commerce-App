9339ca2804a5a0654bd3d0e552fbcf17136bd819
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-06 19:29:31 +0530

    Fix: Product title name changes based on category

--- js/productList.js
+++ js/productList.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             productPrice.classList.add("product-price", "text-center");
 
             productName.textContent = product.title.substring(0, 12) + "..."; 
-            productPrice.textContent = `&#8377; ${product.price}`;
+            productPrice.textContent = `$ ${product.price}`;
 
             const imageInsideProductImage = document.createElement("img");
             imageInsideProductImage.src = product.image;
